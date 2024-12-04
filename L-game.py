5e824bc29ec6c8503e6e0ea083626a912b1c3edf
Sinehan Ezhilmuthu <ezsinehan@gmail.com>
2024-12-03 17:40:49 -0800

    legal moves gen need to change formating to how prof wants

--- L-game.py
+++ L-game.py
@@ -2,7 +2,6 @@ class LGame:
   def __init__(self):
     #init empty 4x4 grid
     self.grid = [['0' for _ in range(4)] for _ in range(4)]
-
     #setting initial pos for p1 and p2 Ls
     self.p1Pos = [(0, 0), (0, 1), (0, 2), (1, 0)]
     self.placePiece(self.p1Pos, 'L1')
@@ -14,10 +13,59 @@ class LGame:
     self.neutralPieces = [(1, 1), (2, 2)]
     self.placeNeutralPieces()
 
-  def placePiece(self, positions, label):
+    self.LPositions = {
+      'N':  [(0, 0), (1, 0), (2, 0), (2, 1)],  # North
+      'NM': [(0, 1), (1, 1), (2, 1), (2, 0)],  # North-Mirrored
+      'E':  [(0, 0), (0, 1), (0, 2), (1, 0)],  # East
+      'EM': [(0, 0), (0, 1), (0, 2), (1, 2)],  # East-Mirrored
+      'S':  [(0, 1), (0, 0), (1, 1), (2, 1)],  # South
+      'SM': [(0, 0), (1, 0), (2, 0), (2, -1)], # South-Mirrored
+      'W':  [(0, 2), (1, 0), (1, 1), (1, 2)],  # West
+      'WM': [(0, 0), (1, 0), (1, 1), (1, 2)],  # West-Mirrored
+    }
+
+  def isValidMove(self, positions):
+    for x, y in positions:
+      if not (0 <= x < 4 and 0 <= y < 4):
+        return False
+
+      if self.grid[x][y] != '0':
+        return False
+    return True
+
+  def genLegalMoves(self, player):
+    legalMoves = []
+    if (player == 'L1'):
+      opponent = 'L2'
+      currPos = self.p1Pos
+    else:
+      opponent = 'L1'
+      currPos = self.p2Pos
+    
+
+    for x, y in currPos:
+      self.grid[x][y] = '0'
+
+    for i in range(4):
+      for j in range(4):
+        for pos in self.LPositions.values():
+          newPos = [(i + x, j + y) for x, y in pos]
+          if self.isValidMove(newPos):
+            legalMoves.append(newPos)
+
+    for x, y in currPos:
+      self.grid[x][y] = player
+
+    legalMoves.remove(currPos)
+    return legalMoves
+
+
+  
+
+  def placePiece(self, positions, player):
     # placing L pieces w proper label
     for x, y in positions:
-      self.grid[x][y] = label
+      self.grid[x][y] = player
 
   def placeNeutralPieces(self):
     # placing neutral pieces w N label
@@ -30,13 +78,8 @@ class LGame:
       print(" | ".join(f"{cell:2}" for cell in row))
       print("-" * 17)
 
-  def isMoveLegal(self, move):
-    return True
-    # check if move is legal
+  
 
-  def makeMove(self, move):
-    #make move
-    return
   
   def startGame(self):
     # starting game
@@ -49,10 +92,7 @@ class LGame:
         print("gg")
         break
 
-      if self.isMoveLegal(move):
-        print(f"You entered: {move}")
-        self.makeMove(move)
-
+      print(self.genLegalMoves('L1'))
       self.printGrid()
 
 if __name__ == "__main__":
